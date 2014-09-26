a7fde05e325835a83450d0072a72d029fa6a921d
Brian Surber <bsurber120@gatech.edu>
2014-09-26 14:58:41 -0400

    started work on marketcontroller

--- src/main/java/hyenas/MarketController.java
+++ src/main/java/hyenas/MarketController.java
@@ -7,6 +7,247 @@ import javafx.fxml.Initializable;
 
 public class MarketController implements Initializable {
 
+	@FXML
+	private Label lWater;
+	
+	@FXML
+	private Label lFurs;
+
+	@FXML
+	private Label lFood;
+	
+	@FXML
+	private Label lOre;
+	
+	@FXML
+	private Label lGames;
+	
+	@FXML
+	private Label lFirearms;
+	
+	@FXML
+	private Label lMedicine;
+	
+	@FXML
+	private Label lMachines;
+
+	@FXML
+	private Label lNarcotics;
+	
+	@FXML
+	private Label lRobots;
+
+	@FXML
+	private Label pWater;
+	
+	@FXML
+	private Label pFurs;
+
+	@FXML
+	private Label pFood;
+	
+	@FXML
+	private Label pOre;
+	
+	@FXML
+	private Label pGames;
+	
+	@FXML
+	private Label pFirearms;
+	
+	@FXML
+	private Label pMedicine;
+	
+	@FXML
+	private Label pMachines;
+
+	@FXML
+	private Label pNarcotics;
+	
+	@FXML
+	private Label pRobots;
+	
+	@FXML
+	private Label nWater;
+	
+	@FXML
+	private Label nFurs;
+
+	@FXML
+	private Label nFood;
+	
+	@FXML
+	private Label nOre;
+	
+	@FXML
+	private Label nGames;
+	
+	@FXML
+	private Label nFirearms;
+	
+	@FXML
+	private Label nMedicine;
+	
+	@FXML
+	private Label nMachines;
+
+	@FXML
+	private Label nNarcotics;
+	
+	@FXML
+	private Label nRobots;
+
+	@FXML
+	private Label cWater;
+	
+	@FXML
+	private Label cFurs;
+
+	@FXML
+	private Label cFood;
+	
+	@FXML
+	private Label cOre;
+	
+	@FXML
+	private Label cGames;
+	
+	@FXML
+	private Label cFirearms;
+	
+	@FXML
+	private Label cMedicine;
+	
+	@FXML
+	private Label cMachines;
+
+	@FXML
+	private Label cNarcotics;
+	
+	@FXML
+	private Label cRobots;
+
+	@FXML
+	private Button bWater;
+	
+	@FXML
+	private Button bFurs;
+
+	@FXML
+	private Button bFood;
+	
+	@FXML
+	private Button bOre;
+	
+	@FXML
+	private Button bGames;
+	
+	@FXML
+	private Button bFirearms;
+	
+	@FXML
+	private Button bMedicine;
+	
+	@FXML
+	private Button bMachines;
+
+	@FXML
+	private Button bNarcotics;
+	
+	@FXML
+	private Button bRobots;
+	
+	@FXML
+	private Button sWater;
+	
+	@FXML
+	private Button sFurs;
+
+	@FXML
+	private Button sFood;
+	
+	@FXML
+	private Button sOre;
+	
+	@FXML
+	private Button sGames;
+	
+	@FXML
+	private Button sFirearms;
+	
+	@FXML
+	private Button sMedicine;
+	
+	@FXML
+	private Button sMachines;
+
+	@FXML
+	private Button sNarcotics;
+	
+	@FXML
+	private Button sRobots;
+	
+	@FXML
+	private Labee eWater;
+	
+	@FXML
+	private Label eFurs;
+
+	@FXML
+	private Label eFood;
+	
+	@FXML
+	private Label eOre;
+	
+	@FXML
+	private Label eGames;
+	
+	@FXML
+	private Label eFirearms;
+	
+	@FXML
+	private Label eMedicine;
+	
+	@FXML
+	private Label eMachines;
+
+	@FXML
+	private Label eNarcotics;
+	
+	@FXML
+	private Label eRobots;
+	
+	@FXML
+	private Label aWater;
+	
+	@FXML
+	private Label aFurs;
+
+	@FXML
+	private Label aFood;
+	
+	@FXML
+	private Label aOre;
+	
+	@FXML
+	private Label aGames;
+	
+	@FXML
+	private Label aFirearms;
+	
+	@FXML
+	private Label aMedicine;
+	
+	@FXML
+	private Label aMachines;
+
+	@FXML
+	private Label aNarcotics;
+	
+	@FXML
+	private Label aRobots;
+	
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     	// TODO: Will implement the buying and selling here.
