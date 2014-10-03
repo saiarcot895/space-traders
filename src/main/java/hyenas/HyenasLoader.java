833aa8fc3c35f6a5f6b35fac1d9a136df215ec45
Saikrishna Arcot <saiarcot895@gmail.com>
2014-10-03 15:48:49 -0400

    Remove tabs in HyenasLoader

--- src/main/java/hyenas/HyenasLoader.java
+++ src/main/java/hyenas/HyenasLoader.java
@@ -9,14 +9,10 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -97,11 +93,11 @@ public class HyenasLoader extends Application {
         }
     }
     
-    public void goToSystemScreen()	{
-    	try	{
-    		changePage("InSystemView.fxml");
-    	} catch (IOException ex)	{
-    		Logger.getLogger(HyenasLoader.class.getName()).log(Level.SEVERE,
+    public void goToSystemScreen() {
+    	try {
+    	    changePage("InSystemView.fxml");
+    	} catch (IOException ex) {
+            Logger.getLogger(HyenasLoader.class.getName()).log(Level.SEVERE,
                     null, ex);
     	}
     }
@@ -110,7 +106,7 @@ public class HyenasLoader extends Application {
         Parent page = FXMLLoader.load(getClass().getResource(pageName));
 
         stage.getScene().setRoot(page);
-  //      stage.sizeToScene();
+//      stage.sizeToScene();
     }
 
     public void confirmSelection() {
