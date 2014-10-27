32228f32412ea1f2b5d6f09e33ea730418e606ad
Saikrishna Arcot <saiarcot895@gmail.com>
2014-10-26 22:49:27 -0400

    Update PlanetTable to new format

--- src/main/java/hyenas/database/PlanetTable.java
+++ src/main/java/hyenas/database/PlanetTable.java
@@ -48,23 +48,20 @@ public class PlanetTable {
         try {
             Statement stmt = conn.createStatement();
             ResultSet planets = stmt.executeQuery("SELECT Planet.Name,"
-                    + " Planet.XPoint, Planet.YPoint, Planet.Radius,"
-                    + " Planet.ClockwiseOrbit, Planet.Size, Planet.Tech,"
-                    + " Planet.Type, SolarSystem.Name"
+                    + " Planet.Radius, Planet.ClockwiseOrbit, Planet.Size,"
+                    + " Planet.Tech, Planet.Type, SolarSystem.Name"
                     + " FROM Planet INNER JOIN SolarSystem"
                     + " ON Planet.SSID = SolarSystem.ID");
             while (planets.next()) {
-                String solarSystemName = planets.getString(9);
+                String solarSystemName = planets.getString(7);
                 SolarSystem solarSystem = Galaxy.getInstance()
                         .getSolarSystemForName(solarSystemName);
 
                 Planet planet = new Planet(planets.getString(1),
-                        planets.getBoolean(5), planets.getInt(7),
-                        planets.getInt(8));
-                planet.setX(planets.getInt(2));
-                planet.setY(planets.getInt(3));
-                planet.setSize(planets.getDouble(6));
-                planet.setOrbitRadius(planets.getInt(4));
+                        planets.getBoolean(3), planets.getInt(5),
+                        planets.getInt(6));
+                planet.setSize(planets.getDouble(4));
+                planet.setOrbitRadius(planets.getInt(2));
 
                 solarSystem.getPlanets().add(planet);
             }
@@ -76,7 +73,6 @@ public class PlanetTable {
     public void createTable() {
         String create = "CREATE TABLE IF NOT EXISTS Planet "
                 + "(ID INTEGER NOT NULL, " + "Name VARCHAR(20) NOT NULL, "
-                + "XPoint INTEGER NOT NULL, " + "YPoint INTEGER NOT NULL, "
                 + "Radius INTEGER NOT NULL, "
                 + "ClockwiseOrbit BOOLEAN NOT NULL, " + "Size DOUBLE NOT NULL, "
                 + "Tech INTEGER NOT NULL, " + "Type INTEGER NOT NULL, "
@@ -92,16 +88,14 @@ public class PlanetTable {
     public void populateTable(Planet planet, SolarSystem system) {
         try {
             PreparedStatement stmt = conn.prepareStatement("INSERT INTO Planet "
-                    + "(Name, XPoint, YPoint, Radius, ClockwiseOrbit, Size, "
+                    + "(Name, Radius, ClockwiseOrbit, Size, "
                     + "Tech, Type, SSID) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
             stmt.setString(1, planet.getPlanetName());
-            stmt.setInt(2, planet.getX());
-            stmt.setInt(3, planet.getY());
-            stmt.setInt(4, planet.getOrbitRadius());
-            stmt.setBoolean(5, planet.isClockwiseOrbit());
-            stmt.setDouble(6, planet.getSize());
-            stmt.setInt(7, planet.getTechLevel());
-            stmt.setInt(8, planet.getPlanetType().ordinal());
+            stmt.setInt(2, planet.getOrbitRadius());
+            stmt.setBoolean(3, planet.isClockwiseOrbit());
+            stmt.setDouble(4, planet.getSize());
+            stmt.setInt(5, planet.getTechLevel());
+            stmt.setInt(6, planet.getPlanetType().ordinal());
 
             PreparedStatement systemStatement = conn.prepareStatement(
                     "SELECT ID FROM SolarSystem WHERE Name = ?");
@@ -111,7 +105,7 @@ public class PlanetTable {
                 throw new IllegalArgumentException();
             }
 
-            stmt.setInt(9, systemIDResultSet.getInt(1));
+            stmt.setInt(7, systemIDResultSet.getInt(1));
             stmt.executeUpdate();
             // Id & ssid are generated based on how the System is generated.
         } catch (SQLException e) {
